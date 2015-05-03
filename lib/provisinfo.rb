6ec524f1619dbfd3d5926fe7d7b3a23b76f2b8f1
Oswaldo Rubio <osrufung@gmail.com>
2015-05-03 17:00:37 +0200

    fixed commander parameters

--- lib/provisinfo.rb
+++ lib/provisinfo.rb
@@ -3,6 +3,7 @@ require 'provisinfo/provisioning'
 
 module Provisinfo
   def self.process(provisioninFileName)
+    puts provisioninFileName
     p1 = Provisioning.new(provisioninFileName)
     puts p1.name
     
--- lib/provisinfo/provisioning.rb
+++ lib/provisinfo/provisioning.rb
@@ -10,13 +10,17 @@ class Provisioning
   #by default, it will load the first .mobileprovision file in current directory
   def initialize(filename = nil)   
     if filename.nil?
-	  @filename = self.class.list_files().first
-	end
-	
+	    @filename = self.class.list_files().first 
+	  else
+      @filename = filename
+    end	   
+  
     if @filename
-	  self.load_from_file(@filename)  	
+	    self.load_from_file(@filename)  	    
+    else
+      @name ="Unknown"
     end
-	
+	  
   end
   
   def load_from_file(filepath)
